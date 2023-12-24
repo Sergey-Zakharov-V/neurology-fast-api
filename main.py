34716634718e038ad5ea57b4e43fe07a2fb00332
Sergey Zakharov <kenthasla@vk.com>
2023-12-24 06:39:20 +0300

    Сделал оплату

--- main.py
+++ main.py
@@ -251,7 +251,7 @@ async def buy_products(payment_data: PaymentSchema):
     payment_data.status = "waiting"
     await UserService.update_key(payment_data.username, str(key))
     await PaymentService.add(**payment_data.model_dump(exclude_none=True))
-    return {"url": payment["confirmation"]["confirmation_url"]}
+    return {"url": json.loads(payment.decode("UTF-8"))["confirmation"]["confirmation_url"]}
 
 
 @app.post("/confirm_payment", include_in_schema=False)
